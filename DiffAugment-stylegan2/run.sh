8641fd885398f355dc674aaf8575f0d7c2e16398
Simone Rossi <simone.rossi@eurecom.fr>
2021-05-24 19:17:42 +0200

    Add error stopping script
    Signed-off-by: Simone Rossi <simone.rossi@eurecom.fr>

--- DiffAugment-stylegan2/run.sh
+++ DiffAugment-stylegan2/run.sh
@@ -8,10 +8,10 @@ mv img_align_celeba datasets/celeba
 pip3 uninstall -y tensorflow tensorflow-probability
 pip3 install tensorflow-gpu==1.15.0 tensorflow-datasets==2.1.0
 
-mkdir datasets/500-celeba
-mkdir datasets/1000-celeba
-mkdir datasets/2000-celeba
-mkdir datasets/4000-celeba
+mkdir -p datasets/500-celeba
+mkdir -p datasets/1000-celeba
+mkdir -p datasets/2000-celeba
+mkdir -p datasets/4000-celeba
 
 python3 prepare_datasets.py
 
